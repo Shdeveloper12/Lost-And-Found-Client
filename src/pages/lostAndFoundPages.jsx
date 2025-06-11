b1400b360d40b12a91917bd97153c0210cc2c51e
Shdevloper <shafayet237@gmail.com>
2025-06-11 23:21:26 +0600

    added lost and found post items recent date

--- src/pages/lostAndFoundPages.jsx
+++ src/pages/lostAndFoundPages.jsx
@@ -1,44 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import { useLoaderData, useNavigation } from 'react-router';
 import AllPostCard from './AllPostCard';
 
-
 const LostAndFoundPages = () => {
-    const initialPost = useLoaderData();
-    const navigation = useNavigation();
-    const [lostandfounditems, setLostandfounditems] = useState(initialPost);
+  const initialPosts = useLoaderData();
+  const navigation = useNavigation();
 
-     if (navigation.state === 'loading') {
-        return (
-            <div className="flex justify-center items-center h-64">
-                <span className="loading loading-ring loading-xl"></span>
-            </div>
-        );
-    }
-    if(!initialPost || initialPost.length === 0){
-        return <div className='text-4xl text-center font-bold '>No Post Found</div>
-    }
+
+  const sortedPosts = useMemo(() => {
+    return [...initialPosts].sort((a, b) => new Date(b.date) - new Date(a.date));
+  }, [initialPosts]);
+
+  const [lostAndFoundItems, setLostAndFoundItems] = useState(sortedPosts);
+
+  if (navigation.state === 'loading') {
     return (
-        <div>
-            <div className='text-center lato my-12  text-2xl font-bold text-orange-500'> All Lost And Found Items</div>
-           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-2  p-5'>
-                {
-
-                    lostandfounditems.map(item => <AllPostCard
-                    key={item._id}
-                    post={item}
-                    setlostandfounditems={setLostandfounditems}
-
-                    
-                    >
-                        
-                    </AllPostCard>)
-                }
-                
-
-           </div>
-        </div>
+      <div className="flex justify-center items-center h-64">
+        <span className="loading loading-ring loading-xl"></span>
+      </div>
     );
+  }
+
+  if (!lostAndFoundItems || lostAndFoundItems.length === 0) {
+    return (
+      <div className="text-4xl text-center font-bold mt-10 text-gray-600">
+        No Posts Found
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1 className="text-center my-12 text-3xl font-bold text-orange-500">
+        All Lost And Found Items (Recent First)
+      </h1>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-2 p-5">
+        {lostAndFoundItems.map((item) => (
+          <AllPostCard
+            key={item._id}
+            post={item}
+            setLostAndFoundItems={setLostAndFoundItems}
+          />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default LostAndFoundPages;
