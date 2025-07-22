993e7e100baa79a6679cb8e0d0414af894f6587d
Shdevloper <shafayet237@gmail.com>
2025-07-22 16:21:01 +0600

    error page setup properly

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,13 +1,28 @@
+import { Link } from "react-router";
 import React from "react";
 import { Helmet } from "react-helmet-async";
-import img1 from "../assets/error.jpg"
+import { FaArrowLeft } from "react-icons/fa";
+import { FaHome } from "react-icons/fa";
+
+
 const Error = () => {
   return (
     <>
       
-      <div className="flex justify-center p-8">
-
-        <img src={img1} alt="" className="w-120"/>
+      <div className="text-center p-8 space-y-4">
+        <h1 className="text-8xl text-gray-400">404</h1>
+        <p className="text-gray-600">Page Not Found</p>
+        <div className="flex justify-center items-center space-x-4">
+          
+          <div>
+            <Link to="/">
+              <button className="flex items-center hover:cursor-pointer bg-blue-500 px-3 text-white hover:bg-blue-600 rounded-md py-2">
+                <FaHome /> Home
+              </button>
+            </Link>
+          </div>
+        </div>
+       
       </div>
     </>
   );
