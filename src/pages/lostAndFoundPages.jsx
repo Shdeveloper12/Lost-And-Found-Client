99ec0a2990148d4ef6dcfdccb0286c59c400264e
Shdevloper <shafayet237@gmail.com>
2025-08-09 19:10:37 +0600

    Lost and found page showing 4 post card on row

--- src/pages/lostAndFoundPages.jsx
+++ src/pages/lostAndFoundPages.jsx
@@ -102,7 +102,7 @@ const LostAndFoundPages = () => {
             No matching posts found.
           </div>
         ) : (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mx-2 p-5">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mx-2 p-5">
             {filteredPosts.map((item) => (
               <AllPostCard
                 key={item._id}
